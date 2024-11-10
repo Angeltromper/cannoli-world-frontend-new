f522cae79a91336c5a595637471849dddd655e2e
Angelique Tromper <99403100+Angeltromper@users.noreply.github.com>
2024-11-10 22:04:57 +0100

    context

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router} from 'react-router-dom';
+import AuthContextProvider from './context/AuthContext.jsx';
+import CannoliContextProvider from './context/CannoliContext.jsx';
+import {CartProvider} from "./context/CartContext.jsx";
+import FormContextProvider from "./context/FormContext.jsx";
+
 import './index.css'
 import App from './App'
 
@@ -8,7 +13,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 
     <React.StrictMode>
         <Router>
-            <App />
+            <AuthContextProvider>
+                <CannoliContextProvider>
+                    <CartProvider>
+                        <FormContextProvider>
+                            <App />
+                        </FormContextProvider>
+                    </CartProvider>
+                </CannoliContextProvider>
+            </AuthContextProvider>
         </Router>
     </React.StrictMode>,
 
